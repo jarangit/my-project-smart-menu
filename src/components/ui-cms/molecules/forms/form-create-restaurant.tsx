61febccf99ea567720e8b5881a09274c202b25e4
jarangit <jaran.dch@gmail.com>
2024-03-29 01:14:32 +0700

    feat: use react userForm in the form create restaurant

--- src/components/ui-cms/molecules/forms/form-create-restaurant.tsx
+++ src/components/ui-cms/molecules/forms/form-create-restaurant.tsx
@@ -7,42 +7,105 @@ import Input from "@ui-cms/atoms/input";
 import React from "react";
 import { FaImage } from "react-icons/fa";
 import { MdOutlineCloudUpload } from "react-icons/md";
+import { useForm, Controller } from "react-hook-form";
 
 type Props = {};
 
+type FormValues = {
+  name: string;
+  facebookUrl: string;
+  googleMapUrl: string;
+  phone: string;
+  lineId: string;
+};
 const FormCreateRestaurant = (props: Props) => {
+  const { handleSubmit, control } = useForm<FormValues>();
+
   return (
-    <Column gap={3}>
-      <div className="relative mb-20  h-[300px] w-full rounded-xl bg-gray-300">
-        <div className="absolute right-4 top-3 cursor-pointer">
-          <MdOutlineCloudUpload size={30} />
-        </div>
-        <div className="absolute -bottom-12 left-6 flex h-36 w-36 cursor-pointer items-center justify-center overflow-hidden rounded-full border-4 border-gray-500 bg-gray-400">
-          <Column className="items-center">
-            <FaImage size={50} />
-            <div>Upload Logo</div>
-          </Column>
-        </div>
-      </div>
-      <Grid gap={6} className="grid-cols-1 gap-y-2 md:grid-cols-2">
-        <Input
-          title="Restaurant name"
-          type="text"
-          placeholder="Restaurant name"
-        />
-        <Input title="Facebook URL" type="text" placeholder="Facebook URL" />
-        <Input title="LineID" type="text" placeholder="LineID" />
-        <Input title="Phone" type="text" placeholder="Phone" />
-        <Input
-          title="Google map URL"
-          type="text"
-          placeholder="Google map URL"
-        />
-      </Grid>
-      <Row className={"justify-end"}>
-        <Button className="w-fit">Next</Button>
-      </Row>
-    </Column>
+    <>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <Column gap={3}>
+          <div className="relative mb-20  h-[300px] w-full rounded-xl bg-gray-300">
+            <div className="absolute right-4 top-3 cursor-pointer">
+              <MdOutlineCloudUpload size={30} />
+            </div>
+            <div className="absolute -bottom-12 left-6 flex h-36 w-36 cursor-pointer items-center justify-center overflow-hidden rounded-full border-4 border-gray-500 bg-gray-400">
+              <Column className="items-center">
+                <FaImage size={50} />
+                <div>Upload Logo</div>
+              </Column>
+            </div>
+          </div>
+          <Grid gap={6} className="grid-cols-1 gap-y-2 md:grid-cols-2">
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <Input
+                  onChange={onChange}
+                  title="Restaurant name"
+                  type="text"
+                  placeholder="Restaurant name"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="facebookUrl"
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <Input
+                  onChange={onChange}
+                  title="Facebook URL"
+                  type="text"
+                  placeholder="Facebook URL"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="lineId"
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <Input
+                  onChange={onChange}
+                  title="LineID"
+                  type="text"
+                  placeholder="LineID"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="googleMapUrl"
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <Input
+                  onChange={onChange}
+                  title="Google Map URL"
+                  type="text"
+                  placeholder="Google Map URL"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="phone"
+              render={({ field: { onChange, onBlur, value, ref } }) => (
+                <Input
+                  onChange={onChange}
+                  title="Phone"
+                  type="text"
+                  placeholder="Phone"
+                />
+              )}
+            />
+          </Grid>
+          <Row className={"justify-end"}>
+            <Button className="w-fit" type="submit">
+              Next
+            </Button>
+          </Row>
+        </Column>
+      </form>
+    </>
   );
 };
 
